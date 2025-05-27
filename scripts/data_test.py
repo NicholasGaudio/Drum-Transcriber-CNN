2c91aad4be8bfd5dd9c8281e78c52d0ed88389e7
Nick Gaudio <gaudio.n@ufl.edu>
2025-05-27 00:39:42 -0400

    some tidying up

--- scripts/data_test.py
+++ scripts/data_test.py
@@ -1,9 +1,9 @@
 from data_prep import audio_to_melspectrogram
 import matplotlib.pyplot as plt
 
-spectrogram = audio_to_melspectrogram("data/closed-hat.wav")
+melspectrogram = audio_to_melspectrogram("data/kick.wav")
 
-plt.imshow(spectrogram, aspect='auto', origin='lower')
+plt.imshow(melspectrogram, aspect='auto', origin='lower')
 plt.colorbar(format='%+2.0f dB')
 plt.title("Mel Spectrogram")
 plt.xlabel("Time")
