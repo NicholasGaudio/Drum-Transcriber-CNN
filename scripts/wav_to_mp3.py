b3db486a6068aa889d262df8e280f9eadff1c1d9
Nick Gaudio <gaudio.n@ufl.edu>
2025-06-18 18:38:10 -0400

    Privitized variables

--- scripts/wav_to_mp3.py
+++ scripts/wav_to_mp3.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
+from env import directory, ffmpeg_path
 
-data_dir = r"insert path"
+data_dir = directory
 
 for root, dirs, files in os.walk(data_dir):
     for file in files:
@@ -9,13 +10,16 @@ for root, dirs, files in os.walk(data_dir):
             wav_path = os.path.join(root, file)
             mp3_path = wav_path[:-4] + ".mp3"
             if os.path.exists(mp3_path): #exists already
+                print("SKIPPING")
                 continue
+            print("CONVERTING")
             subprocess.run([ 
-                r"insert path",
+                ffmpeg_path,
                 "-i", wav_path,
                 "-ab", "192k",
                 "-y",
                 mp3_path
             ])
             # remove the .wav file
+            print("DELETING")
             os.remove(wav_path)
